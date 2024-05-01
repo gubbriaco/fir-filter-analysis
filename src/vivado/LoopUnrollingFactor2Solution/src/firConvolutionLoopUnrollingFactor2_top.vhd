b1da0b505b6dfd44dea49da5278e788175a8aba0
gubbriaco <101352023+gubbriaco@users.noreply.github.com>
2024-05-01 18:59:26 +0200

    Loop Unrolling Factor2 Solution src aggiunti

---
+++ src/vivado/LoopUnrollingFactor2Solution/src/firConvolutionLoopUnrollingFactor2_top.vhd
@@ -0,0 +1,50 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+
+entity firConvolutionLoopUnrollingFactor2_top is
+	port (
+			ap_clk : IN STD_LOGIC;
+			ap_rst : IN STD_LOGIC;
+			ap_start : IN STD_LOGIC;
+			ap_done : OUT STD_LOGIC;
+			ap_idle : OUT STD_LOGIC;
+			ap_ready : OUT STD_LOGIC;
+			inputFilter : IN STD_LOGIC_VECTOR (31 downto 0);
+			outputFilter : OUT STD_LOGIC_VECTOR (31 downto 0);
+			outputFilter_ap_vld : OUT STD_LOGIC 
+  );
+end firConvolutionLoopUnrollingFactor2_top;
+
+architecture Behavioral of firConvolutionLoopUnrollingFactor2_top is
+	
+	component firConvolutionLoopUnrollingFactor2_0 is
+		port (
+			ap_clk : IN STD_LOGIC;
+			ap_rst : IN STD_LOGIC;
+			ap_start : IN STD_LOGIC;
+			ap_done : OUT STD_LOGIC;
+			ap_idle : OUT STD_LOGIC;
+			ap_ready : OUT STD_LOGIC;
+			inputFilter : IN STD_LOGIC_VECTOR (31 downto 0);
+			outputFilter : OUT STD_LOGIC_VECTOR (31 downto 0);
+			outputFilter_ap_vld : OUT STD_LOGIC 
+    );
+  end component;
+	
+	
+	begin
+	
+		firConvolutionLoopUnrollingFactor2_IP : firConvolutionLoopUnrollingFactor2_0 port map(
+				ap_clk => ap_clk,
+				ap_rst => ap_rst,
+				ap_start => ap_start,
+				ap_done => ap_done,
+				ap_idle => ap_idle,
+				ap_ready => ap_ready,
+				inputFilter => inputFilter,
+				outputFilter => outputFilter,
+				outputFilter_ap_vld => outputFilter_ap_vld
+		);
+
+end Behavioral;
