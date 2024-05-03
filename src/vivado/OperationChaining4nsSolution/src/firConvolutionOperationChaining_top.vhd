779fe5eb5adb66c51b62b51ea3998b7c82db1aa6
gubbriaco <101352023+gubbriaco@users.noreply.github.com>
2024-05-03 16:03:31 +0200

    Operation Chaining 4ns Solution src aggiunti

---
+++ src/vivado/OperationChaining4nsSolution/src/firConvolutionOperationChaining_top.vhd
@@ -0,0 +1,50 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+
+entity firConvolutionOperationChaining_top is
+	port (
+			ap_clk : IN STD_LOGIC;
+			ap_rst : IN STD_LOGIC;
+			ap_start : IN STD_LOGIC;
+			ap_done : OUT STD_LOGIC;
+			ap_idle : OUT STD_LOGIC;
+			ap_ready : OUT STD_LOGIC;
+			inputFilter : IN STD_LOGIC_VECTOR (31 downto 0);
+			outputFilter : OUT STD_LOGIC_VECTOR (31 downto 0);
+			outputFilter_ap_vld : OUT STD_LOGIC 
+  );
+end firConvolutionOperationChaining_top;
+
+architecture Behavioral of firConvolutionOperationChaining_top is
+	
+	component firConvolutionOperationChaining_0 is
+		port (
+			ap_clk : IN STD_LOGIC;
+			ap_rst : IN STD_LOGIC;
+			ap_start : IN STD_LOGIC;
+			ap_done : OUT STD_LOGIC;
+			ap_idle : OUT STD_LOGIC;
+			ap_ready : OUT STD_LOGIC;
+			inputFilter : IN STD_LOGIC_VECTOR (31 downto 0);
+			outputFilter : OUT STD_LOGIC_VECTOR (31 downto 0);
+			outputFilter_ap_vld : OUT STD_LOGIC 
+    );
+  end component;
+	
+	
+	begin
+	
+		firConvolutionOperationChaining_IP : firConvolutionOperationChaining_0 port map(
+				ap_clk => ap_clk,
+				ap_rst => ap_rst,
+				ap_start => ap_start,
+				ap_done => ap_done,
+				ap_idle => ap_idle,
+				ap_ready => ap_ready,
+				inputFilter => inputFilter,
+				outputFilter => outputFilter,
+				outputFilter_ap_vld => outputFilter_ap_vld
+		);
+
+end Behavioral;
