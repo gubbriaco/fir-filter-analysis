53a39bb11ae05cf6c292c67b62e14f50630b293f
gubbriaco <101352023+gubbriaco@users.noreply.github.com>
2024-05-15 18:01:05 +0200

    Loop Unrolling Factor 4 vivado src aggiornati

--- src/vivado/LoopUnrollingFactor4Solution/sim/firConvolutionLoopUnrollingFactor4_tb.vhd
+++ src/vivado/LoopUnrollingFactor4Solution/sim/firConvolutionLoopUnrollingFactor4_tb.vhd
@@ -36,7 +36,7 @@ architecture Behavioral of firConvolutionLoopUnrollingFactor4_tb is
 	signal outputFilter :  STD_LOGIC_VECTOR (31 DOWNTO 0);
 	signal outputFilter_ap_vld :  STD_LOGIC;
 
-	signal cycles: integer := 25;
+	signal cycles: integer := 59;
 
 
 	begin
