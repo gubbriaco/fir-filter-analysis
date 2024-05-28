205a7c6efdc246fc9b0d591cfca7272d6538f178
gubbriaco <101352023+gubbriaco@users.noreply.github.com>
2024-05-28 10:59:06 +0200

    Operation Chaining 5ns vivado src aggiornati

--- src/vivado/OperationChaining5nsSolution/sim/firConvolutionOperationChaining_tb.vhd
+++ src/vivado/OperationChaining5nsSolution/sim/firConvolutionOperationChaining_tb.vhd
@@ -36,7 +36,7 @@ architecture Behavioral of firConvolutionOperationChaining_tb is
 	signal outputFilter :  STD_LOGIC_VECTOR (31 DOWNTO 0);
 	signal outputFilter_ap_vld :  STD_LOGIC;
 
-	signal cycles: integer := 44;
+	signal cycles: integer := 88;
 
 
 	begin
